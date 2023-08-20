f0df487a6da1b9e42c4dff13439eac7408a5695b
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2023-08-20 10:10:02 +0530

    Added Real time detection

--- predict.py
+++ predict.py
@@ -1,38 +1,46 @@
 import cv2
-from PIL import Image
 from ultralytics import YOLO
 
 model = YOLO("best.pt")
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH , 320)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT , 240) 
+cap.set(cv2.CAP_PROP_FRAME_WIDTH , 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT , 480) 
 
 
+i = 1
 while cap.isOpened():
     ret, frame = cap.read()
 
+    i += 1
+
+
     if not ret:
         break
-    cv2.imshow('webcam feed', frame)
 
-    # try:
-    #     results = model.predict(frame)
-    #     for r in results:
-    #         x1, y1, x2, y2 = map(int, r.boxes.xyxy[0][:4])
-    #         conf = float(r.boxes.conf)
-    #         cls = int(r.boxes.cls)
+    try:
+        if i % 50 == 0: 
+            results = model.predict(frame)
+
+        for r in results:
+            x1, y1, x2, y2 = map(int, r.boxes.xyxy[0][:4])
+            conf = float(r.boxes.conf)
+            cls = int(r.boxes.cls)
             
-    #         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-    #         cv2.putText(frame, f"{cls} {conf:.2f}", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv2.putText(frame, f"{cls} {conf:.2f}", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            
+            roi = frame[y1:y2, x1:x2]
 
+    except Exception as e:
+        continue
 
-    # except Exception as e:
-    #     print(e)
-    #     continue
+    cv2.imshow("frame", frame)
+    key = cv2.waitKey(50)  # Store the key pressed
 
-    if cv2.waitKey(50) & 0xFF == ord('q'):  # Delay of 50 milliseconds
+    if key == 27:  # Check for the 'Esc' key
         break
+
 
 cap.release()
 cv2.destroyAllWindows()
