0c68a8573262b9482e223d2ba7e4162a224102b6
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2023-08-24 07:37:37 +0530

    Recognizing text using easyocr

--- predict.py
+++ predict.py
@@ -1,5 +1,9 @@
 import cv2
 from ultralytics import YOLO
+import easyocr
+import numpy as np
+reader=easyocr.Reader(["en","hi"])
+
 
 model = YOLO("best.pt")
 
@@ -19,28 +23,28 @@ while cap.isOpened():
         break
 
     try:
-        if i % 50 == 0: 
+        if i % 10 == 0: 
             results = model.predict(frame)
+            roi = frame[y1:y2, x1:x2]
+            text=reader.readtext(roi)
 
         for r in results:
             x1, y1, x2, y2 = map(int, r.boxes.xyxy[0][:4])
             conf = float(r.boxes.conf)
             cls = int(r.boxes.cls)
-            
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
             cv2.putText(frame, f"{cls} {conf:.2f}", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            print("Text:", text[0][1])
+
             
-            roi = frame[y1:y2, x1:x2]
 
     except Exception as e:
         continue
 
     cv2.imshow("frame", frame)
-    key = cv2.waitKey(50)  # Store the key pressed
 
-    if key == 27:  # Check for the 'Esc' key
+    if cv2.waitKey(1) == ord("q"):
         break
-
 
 cap.release()
 cv2.destroyAllWindows()
